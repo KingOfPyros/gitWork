0def618743907084880422d8efab538d13a0ecc1
student <student@COMP-507-9>
2021-09-11 11:29:35 +0300

    add : countOf

--- gitWork/gitWork.cpp
+++ gitWork/gitWork.cpp
@@ -1,34 +1,17 @@
-﻿// gitWork.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
 #include "MyList_Decor.h"
+
 int main()
 {
-	/*MyList<int> ml;
 
-	ml.push_back(3);
-	ml.push_back(2);
-	ml.push_back(1);
-
-	std::cin >> ml;
-
-	std::cout << ml << "\n";
-	std::cout << ml.sum() << "\n";
-	std::cout << ml.sum(1) << "\n";
-	std::cout << ml.isHasOdd() << "\n";*/
 
 	MyList_Decor<int> ml;
 
-	ml.push_back(3);
+	ml.push_back(1);
 	ml.push_back(2);
 	ml.push_back(1);
 
 	std::cin >> ml;
 
-	std::cout << ml << "\n";
-	std::cout << ml.sum() << "\n";
-	std::cout << ml.sum(1) << "\n";
-	std::cout << ml.isHasOdd() << "\n";
 }
